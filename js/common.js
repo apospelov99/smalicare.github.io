6b04e6c4189026f755fdb9c0a198fcc2a48368f9
apospelov13 <small99@hotbox.ru>
2017-06-08 17:26:45 +0300

    v2

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+	// Custom JS
+/* MenuToggle
+	$('.menuToggle').on('click', function(){
+		$('.menu').slideToggle(300, function() {
+			if ($(this).css('display') ==='none'){
+				$(this).removeAttr('syle');
+			}
+		})
+	});
+*/
+});
